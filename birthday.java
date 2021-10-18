72c44bf5d208f99976ce357680501ec918783117
LanzsGit <88616420+LanzsGit@users.noreply.github.com>
2021-10-18 20:40:38 +0100

    Update birthday.java

--- birthday.java
+++ birthday.java
@@ -7,27 +7,27 @@ public class birthday {
         int n = scanner.nextInt();
         int x = scanner.nextInt();
         scanner.close();
-        int sum_prob = 0;
+        double sum_prob = 0;
 
-        for(int i = 0; i <= 100000; i++){
+        for(int i = 0; i <= 1000000; i++){
             sum_prob = sum_prob + birthday_loop(n, x);
         }
-        System.out.print(sum_prob/1000);
+        System.out.print(sum_prob/1000000.00);
     }
     public static int birthday_loop(int n, int x){
         int[] array = new int[n];
-        for(int i = 0; i <= array.length - 1; i++){             //filling array with random integers between 365
+        for(int i = 0; i <= array.length - 1; i++){             //filling array with random integers between 1 and 365
             array[i] = (int)(Math.random()*((365 - 1) + 1));
         }
         int count = 0;
         for(int i = 0; i <= array.length - 1; i++){             //nested loop to check if any values in the array are equal
-            for(int j = 1; j <= array.length - 1; j++){
+            for(int j = 0; j <= array.length - 1; j++){
                 if(array[i] == array[j] && i != j){
                     count = count + 1;
                 }
             }
         }
-        if(count >= x - 1){                                     //if x or more students are found to have the same birthday, return 1
+        if(count >= x){                                     //if x or more students are found to have the same birthday, return 1
             return 1;
         }
         else{
