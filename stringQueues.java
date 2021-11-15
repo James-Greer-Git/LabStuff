7af1e580f6a55b94000131b8ae91558cf36f1de8
LanzsGit <88616420+LanzsGit@users.noreply.github.com>
2021-11-15 21:55:21 +0000

    Update stringQueues.java

--- stringQueues.java
+++ stringQueues.java
@@ -4,22 +4,22 @@ public class stringQueues{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         Queue<String> q = new LinkedList<String>();
-        int instructions = sc.nextInt();
+        //int instructions = sc.nextInt();
 
         do{
             String line = sc.next();
             if(line.equals("INSERT")){
                 q.add(sc.next());
-                instructions--;
+                //instructions--;
             }
             else if(line.equals("REMOVE") && !q.isEmpty()){
                 q.remove();
-                instructions--;
+                //instructions--;
             }
             else{
-                instructions--;
+                //instructions--;
             }
-        }while(instructions > 0);
+        }while(sc.hasNextLine()/*instructions > 0*/);
 
         sc.close();
 
