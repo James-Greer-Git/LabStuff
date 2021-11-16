7d03c8f37223d73a762509292d77e0002991acf7
LanzsGit <88616420+LanzsGit@users.noreply.github.com>
2021-11-16 10:04:37 +0000

    Update stringQueues.java

--- stringQueues.java
+++ stringQueues.java
@@ -1,41 +1,49 @@
 import java.util.*;
 
-public class stringQueues{
+public class Solution{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         Queue<String> q = new LinkedList<String>();
         //int instructions = sc.nextInt();
 
-        do{
+        while(sc.hasNext()){
             String line = sc.next();
-            if(line.equals("INSERT")){
-                q.add(sc.next());
-                //instructions--;
-            }
-            else if(line.equals("REMOVE") && !q.isEmpty()){
-                q.remove();
-                //instructions--;
+            
+            if(q.isEmpty()){
+                if(line.equals("INSERT")){
+                    q.add(sc.next());
+                }
+                else{
+                    
+                }
             }
             else{
-                //instructions--;
+                if(line.equals("INSERT")){
+                    q.add(sc.next());
+                }
+                else if(line.equals("REMOVE")){
+                        q.remove();
+                    }
+                }
             }
-        }while(sc.hasNextLine()/*instructions > 0*/);
-
         sc.close();
 
         int size = q.size();
-
-        if(size%2 == 0){
-            do{
-               q.remove();
-            }
-            while(q.size() - 1 > size/2);
+        
+        if(q.isEmpty()){
+            System.out.print("empty");
+            System.exit(0);
         }
-        else{
-            do{
+
+        else if(size%2 == 0){
+            while(q.size() - 1 > size/2){
                 q.remove();
             }
-            while(q.size() - 1 > size/2);
+        }
+        else{
+            while(q.size() - 1 > size/2){
+                q.remove();
+            }
         }
         System.out.print(q.peek());
     }
