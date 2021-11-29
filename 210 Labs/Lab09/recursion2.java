4286db6e79ce354ca0f9472de10a4afa3b277c7e
James Greer <88616420+James-Greer-Git@users.noreply.github.com>
2021-11-29 15:58:57 +0000

    Update recursion2.java

--- 210 Labs/Lab09/recursion2.java
+++ 210 Labs/Lab09/recursion2.java
@@ -1,13 +1,16 @@
+import java.util.Scanner;
 public class recursion2{
     public static void main(String args[]){
-        System.out.print(function(7, 1.3, 100));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(function(scanner.nextInt(), scanner.nextDouble(), scanner.nextDouble()));
+        scanner.close();
     }
-    public static double function(double x, double i, int y){     //x : account balance, i : interest rate, y : number of years
+    public static double function(int y, double i, double x){     //x : account balance, i : interest rate, y : number of years
         if(x == 0 || x < 0){
             return 0.0;
         }
         else if(y > 0){
-            return function(x * (1 + i/100), i, y - 1);
+            return function(y - 1, i, x * (1 + i/100));
         }
         else{
             return x;
