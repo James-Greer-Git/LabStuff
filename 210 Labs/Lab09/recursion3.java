18069384b98684fb8f40e7aef8c90db02d0dc112
James Greer <88616420+James-Greer-Git@users.noreply.github.com>
2021-11-29 16:00:04 +0000

    Update recursion3.java

--- 210 Labs/Lab09/recursion3.java
+++ 210 Labs/Lab09/recursion3.java
@@ -1,6 +1,9 @@
+import java.util.Scanner
 public class recursion3{
     public static void main(String args[]){
-        System.out.print(1 - function(23));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(1 - function(scanner.nextInt()));
+        scanner.close();
     }
     public static double function(int n){
         if(n == 1){
