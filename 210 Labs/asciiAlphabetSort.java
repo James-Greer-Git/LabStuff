f8140af5be08045e49b3a9ea430b3c4a34050ee1
James Greer <88616420+James-Greer-Git@users.noreply.github.com>
2021-12-07 10:52:15 +0000

    Update asciiAlphabetSort.java

--- 210 Labs/asciiAlphabetSort.java
+++ 210 Labs/asciiAlphabetSort.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 public class Solution{
     public static String[] myArray;
@@ -26,31 +30,25 @@ public class Solution{
 
 
         for(int i = 0 ; i < myArray.length; i++){
-            System.out.println(asciiArray[i] + ": " + myArray[i]);
+            System.out.println(/*asciiArray[i] + ": " + */myArray[i]);
         }
     }
     public static void sort(){
         ascii();
         recMergeSort(asciiWorkSpace, lowerBound, upperbound);
-        for(int i = 0; i < myArray.length; i++){
-            int start_index = i;
-            for(int j = i + 1; j < myArray.length; j++){
-                if(asciiArray[i] == asciiArray[j]){
-                }
-                if(asciiArray[i] != asciiArray[j] || j == myArray.length - 1){
-                    int stop_index = j + 1;
-                    Arrays.sort(myArray, start_index, stop_index);
-                }
-            }
-        }
 
-        for(int i = 0; i < myArray.length; i++){
-            for(int j = myArray.length - 1; j >= i; j--){
-                if(asciiArray[i] == asciiArray[j]){
-                    String temp = myArray[i];
-                    myArray[i] = myArray[j];
-                    myArray[j] = temp;
-                    i++;
+        List<String> reverseList = new ArrayList<String>();
+
+        for(int i = 0; i < myArray.length - 1; i++){
+            if(asciiArray[i] == asciiArray[i + 1]){
+                reverseList.add(myArray[i]);
+            }
+            if(asciiArray[i] != asciiArray[i + 1] || i == myArray.length - 2){
+                reverseList.add(myArray[i + 1]);
+                Collections.sort(reverseList, Collections.reverseOrder());
+                for(int j = 0; j < myArray.length; j++){
+                    myArray[j] = reverseList.get(0);
+                    reverseList.remove(0);
                 }
             }
         }
@@ -78,36 +76,42 @@ public class Solution{
         int lowerBound = first;
         int mid = second - 1;
         int n = upperbound - lowerBound + 1;
+        int[] localAsciiArray = asciiArray;
+        int[] localAsciiWorkSpace = asciiWorkSpace;
+        String[] localStringWorkSpace = stringWorkSpace;
+        String[] localMyArray = myArray;
 
         while(first <= mid && second <= upperbound){
-            if(asciiArray[first] < asciiArray[second]){
-                asciiWorkSpace[j] =  asciiArray[first];
-                stringWorkSpace[j] = myArray[first];
+            if(localAsciiArray[first] < localAsciiArray[second]){
+                localAsciiWorkSpace[j] =  localAsciiArray[first];
+                localStringWorkSpace[j] = localMyArray[first];
                 j++;
                 first++;
             }
             else{
-                asciiWorkSpace[j] = asciiArray[second];
-                stringWorkSpace[j] = myArray[second];
+                localAsciiWorkSpace[j] = localAsciiArray[second];
+                localStringWorkSpace[j] = localMyArray[second];
                 j++;
                 second++;
             }
         }
         while(first <= mid){
-            asciiWorkSpace[j] = asciiArray[first];
-            stringWorkSpace[j] = myArray[first];
+            localAsciiWorkSpace[j] = localAsciiArray[first];
+            localStringWorkSpace[j] = localMyArray[first];
             j++;
             first++;
         }
         while(second <= upperbound){
-            asciiWorkSpace[j] = asciiArray[second];
-            stringWorkSpace[j] = myArray[second];
+            localAsciiWorkSpace[j] = localAsciiArray[second];
+            localStringWorkSpace[j] = localMyArray[second];
             j++;
             second++;
         }
         for(j = 0; j < n; j++){
-            asciiArray[lowerBound + j] = asciiWorkSpace[j];
-            myArray[lowerBound + j] = stringWorkSpace[j];
+            localAsciiArray[lowerBound + j] = localAsciiWorkSpace[j];
+            localMyArray[lowerBound + j] = localStringWorkSpace[j];
         }
+        myArray = localMyArray;
+        asciiArray = localAsciiArray;
     }
 }
